f75ee529704c8f45c238cc9159266be33b9581b0
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-02-17 11:34:53 -0300

    feat: created new dynamic category component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,35 @@
+import { CategoryButton } from "@/components/categoryButton";
 import { Header } from "@/components/header";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { CATEGORIES } from "@/utils/data/products";
+import { useState } from "react";
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
     <View className="flex-1 pt-8">
-      <Header title="Faça seu pedido" cartQuantityItems={0} />
+      <Header title="Faça seu pedido" cartQuantityItems={3} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-w-10 mt-5"
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 }
