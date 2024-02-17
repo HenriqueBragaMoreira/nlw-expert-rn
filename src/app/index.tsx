c9bc1ed09ef52e45af8bae8be592f85c7e997960
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-02-17 10:47:40 -0300

    feat: created a new dynamic header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,10 @@
+import { Header } from "@/components/header";
 import { View, Text } from "react-native";
 
 export default function Home() {
-  return <View className="flex-1"></View>;
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantityItems={0} />
+    </View>
+  );
 }
