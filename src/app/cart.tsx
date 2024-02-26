61537bf8e71f7f3721b560a72d8cf38720d0c821
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-02-25 22:06:50 -0300

    feat(cart): created logic for removing products from the cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/components/header";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Alert } from "react-native";
 import { Product } from "@/components/product";
-import { useCartStore } from "@/stores/cartStore";
+import { ProductCartProps, useCartStore } from "@/stores/cartStore";
 import { formatCurrency } from "@/utils/functions/formatCurrency";
 import { Input } from "@/components/input";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
@@ -19,6 +19,18 @@ export default function Cart() {
     )
   );
 
+  function handleProductRemove(product: ProductCartProps) {
+    Alert.alert("Remover", `Deseja remover ${product.title} do carrinho ?`, [
+      {
+        text: "Cancelar",
+      },
+      {
+        text: "Remover",
+        onPress: () => cartStore.remove(product.id),
+      },
+    ]);
+  }
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
@@ -28,7 +40,11 @@ export default function Cart() {
             {cartStore.products.length > 0 ? (
               <View className="border-b border-slate-700">
                 {cartStore.products.map((product) => (
-                  <Product key={product.id} data={product} />
+                  <Product
+                    key={product.id}
+                    data={product}
+                    onPress={() => handleProductRemove(product)}
+                  />
                 ))}
               </View>
             ) : (
--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -9,6 +9,7 @@ export type ProductCartProps = ProductProps & {
 type StateProps = {
   products: ProductCartProps[];
   add: (product: ProductProps) => void;
+  remove: (productId: string) => void;
 };
 
 export const useCartStore = create<StateProps>((set) => ({
@@ -16,5 +17,9 @@ export const useCartStore = create<StateProps>((set) => ({
   add: (product: ProductProps) =>
     set((state) => ({
       products: cartInMemory.add(state.products, product),
+    })),
+  remove: (productId: string) =>
+    set((state) => ({
+      products: cartInMemory.remove(state.products, productId),
     })),
 }));
