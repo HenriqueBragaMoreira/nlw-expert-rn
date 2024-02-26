2104a16db515186fb4e8dcbb2d6a9539ecb6e3c1
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-02-26 07:24:52 -0300

    feat(cart): saving cart products in memory

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,5 +1,7 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as cartInMemory from "./helpers/cartInMemory";
 
 export type ProductCartProps = ProductProps & {
@@ -12,14 +14,22 @@ type StateProps = {
   remove: (productId: string) => void;
 };
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
-  remove: (productId: string) =>
-    set((state) => ({
-      products: cartInMemory.remove(state.products, productId),
-    })),
-}));
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+    }),
+    {
+      name: "nlw-expert:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
